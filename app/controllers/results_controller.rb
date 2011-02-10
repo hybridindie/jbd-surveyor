4abcad9df04e844c43ac3761c5b5161fdc3c9d24
John Brien <iam@hybridindie.com>
2011-02-10 11:52:59 -0500

    moved client specific out of the gem

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -12,16 +12,16 @@ class ResultsController < ApplicationController
     @questions = @survey.sections_with_questions.map(&:questions).flatten
     respond_to do |format|
       format.html
-      format.csv { export_csv(@projects) }
+#      format.csv { export_csv(@response_sets) }
     end
   end
 
 protected
 
-  def export_csv(projects)
-    filename = I18n.l(Time.now, :format => :short) + "- Projects.csv"
-    content = Project.to_csv(projects)
-    content = BOM + Iconv.conv("utf-16le", "utf-8", content)
-    send_data content, :filename => filename
-  end
+#  def export_csv(projects)
+#    filename = I18n.l(Time.now, :format => :short) + "- Reports.csv"
+#    content = Project.to_csv(projects)
+#    content = BOM + Iconv.conv("utf-16le", "utf-8", content)
+#    send_data content, :filename => filename
+#  end
 end
